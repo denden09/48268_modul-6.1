7c759331cb289cc4c734602b4ed6864cd56fc4fe
zerox <zeroxleashgaming@gmail.com>
2025-03-13 21:18:04 +0700

    stp 5

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -16,13 +16,20 @@
 
 package com.example.inventory.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
-    val id: Int = 0,
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val name: String,
     val price: Double,
     val quantity: Int
 )
+
+data class User(val firstName: String, val lastName: String){
+}
